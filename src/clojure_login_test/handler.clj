d2ea7035118d62db842d78aa3cb584667c2f26a0
Gel <gel@leandro@alertaapp.com.br>
2018-07-31 01:47:10 -0300

    creating neocons connection and node creation functions

--- src/clojure_login_test/handler.clj
+++ src/clojure_login_test/handler.clj
@@ -1,10 +1,20 @@
 (ns clojure-login-test.handler
-  (:require [compojure.core :refer :all]
+  (:require [clojurewerkz.neocons.rest :as nr]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def conn (nr/connect "http://localhost:7474/db/data/"))
+
+(defn create-node
+  [node]
+  (conn nn/create node))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (POST "/api/v1/user" [username email password] (create-node {:username username
+                                                               :email email
+                                                               :password password}))
   (route/not-found "Not Found"))
 
 (def app
